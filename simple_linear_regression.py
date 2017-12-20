6aa80e18a06dd517657f0e6ba2037ef5d895358c
avulasaikiranreddy <avulasaikiranreddy@gmail.com>
2017-12-21 00:14:28 +0530

    indentation update

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -25,6 +25,7 @@ from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
 
 
+
 # Fitting simple linear regression to training set.
 
 from sklearn.linear_model import LinearRegression
